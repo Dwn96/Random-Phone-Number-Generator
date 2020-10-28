00f9fe12fc992add16da80a0e199dc1b330741cf
Dan96 <wambu43@gmail.com>
2020-10-28 18:34:18 +0300

    first commit

---
+++ Genr.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class Genr {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+//Rand numbers between	720000000 - 729999999
+
+	int max= 729999999;
+	int min= 720000000;
+		
+	Random rn = new Random();
+	
+	
+
+	int range = max - min + 1;
+	
+	
+	for (int i =0; i<10; i++) {
+		
+		int randomNum = rn.nextInt(range) + min;
+		int numberSet[]= new int[10];
+		numberSet[i] = randomNum;		
+		
+		System.out.println("+254"+numberSet[i]);
+	}
+	
+	
+	
+
+	
+	
+	
+	
+		
+	}
+
+}
